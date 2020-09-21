00c70fb5d9cd4e9d81c65277bd1b28e8cbbe964b
cloudiebro@hotmail.com <cloudiebro@hotmail.com>
2020-09-20 19:06:40 -0700

    adding solution

---
+++ Easy/Python3/1448_Running_Sum_1d_Array.py
@@ -0,0 +1,20 @@
+from typing import List
+#Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+
+#Return the running sum of nums.
+ 
+#Example 1:
+
+#Input: nums = [1,2,3,4]
+#Output: [1,3,6,10]
+#Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
+
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        
+        Output = []
+        Output.append(nums[0])
+
+        for i in range(len(nums)-1):
+            Output.append(Output[i] + nums[i+1])
+        return Output
