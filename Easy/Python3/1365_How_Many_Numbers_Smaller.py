dc2b5a08438e9313ad66be30a7bf20ab0a3661d2
cloud971 <chasam@pdx.edu>
2020-09-26 02:43:48 -0700

    brute force change

--- Easy/Python3/1365_How_Many_Numbers_Smaller.py
+++ Easy/Python3/1365_How_Many_Numbers_Smaller.py
@@ -1,3 +1,4 @@
+from typing import List
 #Given the array nums, for each nums[i] find out how many numbers in the array are smaller than it.
 #That is, for each nums[i] you have to count the number of valid j's such that j != i and nums[j] < nums[i].
 
@@ -13,3 +14,14 @@
 #For nums[2]=2 there exist one smaller number than it (1). 
 #For nums[3]=2 there exist one smaller number than it (1). 
 #For nums[4]=3 there exist three smaller numbers than it (1, 2 and 2).
+class Solution:
+    def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
+        # BRUTE FORCE
+        #to_return = []
+        #for x in range(len(nums)):
+        #    to_add = 0
+        #    for y in range(len(nums)):
+        #        if nums[x] > nums[y]:
+        #            to_add+= 1
+        
+        #return to_return
