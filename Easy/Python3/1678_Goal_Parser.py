40bbebe43c55a83597f3d51b76fae4222a664d7f
cloudiebro@hotmail.com <cloudiebro@hotmail.com>
2020-12-21 15:36:29 -0800

    1678 initial commit

---
+++ Easy/Python3/1678_Goal_Parser.py
@@ -0,0 +1,48 @@
+# You own a Goal Parser that can interpret a string command. The command 
+# consists of an alphabet of "G", "()" and/or "(al)" in some order. The Goal 
+# Parser will interpret "G" as the string "G", "()" as the string "o", and
+# "(al)" as the string "al". The interpreted strings are then concatenated in
+# the original order.
+
+# Given the string command, return the Goal Parser's interpretation of command.
+
+# Example 1:
+
+# Input: command = "G()(al)"
+# Output: "Goal"
+# Explanation: The Goal Parser interprets the command as follows:
+# G -> G
+# () -> o
+# (al) -> al
+# The final concatenated result is "Goal".
+
+# Example 2:
+
+# Input: command = "G()()()()(al)"
+# Output: "Gooooal"
+
+# Example 3:
+
+# Input: command = "(al)G(al)()()G"
+# Output: "alGalooG"
+
+class Solution:
+    def interpret(self, command: str) -> str:
+        tracking = ""
+        create = ""
+        
+        for x in command:
+            if x == 'G':
+                create += x
+            elif x == ')':
+                if tracking == '(':
+                    create += 'o'
+                    tracking = ""
+                else:
+                    create += 'al'
+                    tracking = ""
+            else:
+                tracking += x
+        
+        return create
+                
