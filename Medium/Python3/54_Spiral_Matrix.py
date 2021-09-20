1d9ffae6e861e4569565e2d61bc85ebdf4d8a0d7
unknown <cloudiebro@hotmail.com>
2021-09-19 18:00:06 -0700

    added comments for problem description

--- Medium/Python3/54_Spiral_Matrix.py
+++ Medium/Python3/54_Spiral_Matrix.py
@@ -1,5 +1,22 @@
 from typing import List
 
+# Given an m x n matrix, return all elements of the matrix in spiral order.
+#
+# Example 1:
+#
+# 1 2 3
+# 4 5 6
+# 7 8 9
+# becomes 1 2 3 6 9 8 7 4 5
+#
+# Example 2:
+#
+# 1  2  3  4
+# 5  6  7  8
+# 9  10 11 12
+# 13 14 15 16
+#
+# Becomes 1 2 3 4 8 12 16 15 14 13 9 5 6 7 11 10
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
         to_return = []
