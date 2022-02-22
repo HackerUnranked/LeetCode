99ae6facfbe616c5fbce8c603fda61892f77272b
unknown <cloudiebro@hotmail.com>
2022-02-22 13:16:28 -0800

    refactored so i can understand better

--- Easy/Python3/1_Two_Sum.py
+++ Easy/Python3/1_Two_Sum.py
@@ -9,13 +9,16 @@ from typing import List
 #Input: nums = [2,7,11,15], target = 9
 #Output: [0,1]
 #Output: Because nums[0] + nums[1] == 9, we return [0, 1].
-
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        hash_table = dict()
-        for i in range(len(nums)):
-            compliment = target - nums[i]
-            if compliment not in hash_table:
-                hash_table[nums[i]] = i
+        my_dic = {}
+        
+        for x,y in enumerate(nums):
+            if target - y not in my_dic:
+                # keep the value as the key and the index and the value
+                # if we don't have it in the dictionary yet
+                my_dic[y] = x
+            # we have it in the dictionary
             else:
-                return [hash_table[compliment], i]
+                return [my_dic[target-y],x]
+                
