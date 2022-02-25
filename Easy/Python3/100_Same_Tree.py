f0d65d9609e6c9afb4b6432229fd10b1e6049fd3
unknown <cloudiebro@hotmail.com>
2022-02-24 17:39:43 -0800

    minor changes and rebased to match master

--- Easy/Python3/100_Same_Tree.py
+++ Easy/Python3/100_Same_Tree.py
@@ -10,46 +10,52 @@ class TreeNode:
 class Solution:
     def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
         
-        if p == None and q == None:
-            print("hello word")
+        # if they are both Null then return true
+        if p == q and q == None:
             return True
+        # only one of them is None so we return false
         if p == None:
             return False
         if q == None:
             return False
         
-        a_root = []
-        b_root = []
+        list_1 = []
+        list_2 = []
         
-        a_root.append(p)
-        b_root.append(q)
-
-        # we use preorder traversal in iterative form, first visit the parent
-        # then the children        
-        while len(a_root) > 0 and len(b_root) > 0:
-            a = a_root.pop()
-            b = b_root.pop()
+        # visit the parent first
+        list_1.append(p)
+        list_2.append(q)
+        
+        # User preorder traversal and visit the children node
+        # to compare if the trees are the same
+        while len(list_1) > 0 and len(list_2) > 0:
+            # grab the node in stack to compare
+            root_1 = list_1.pop()
+            root_2 = list_2.pop()
             
-            if a.val != b.val:
+            # the Values don't match return false
+            if root_1.val != root_2.val:
                 return False
-            if a.right and b.right:
-                a_root.append(a.right)
-                b_root.append(b.right)
-            if b.left and a.left:
-                a_root.append(a.left)
-                b_root.append(b.left)
+            # the second tree has a right child but the first tree doesn't
+            if root_2.right and root_1.right == None:
+                return False
+            # if the second tree has a left child but the first tree doesn't
+            if root_2.left and root_1.left == None:
+                return False
+            # if the first tree has a right child but the second tree doesn't
+            if root_1.right and root_2.right == None:
+                return False
+            # if the first tree has a left child but the second tree doesn't
+            if root_1.left and root_2.left == None:
+                return False
             
-            # b has a right but a doesnt
-            if b.right and a.right == None:
-                return False
-            # b has a left but a doesnt
-            if b.left and a.left == None:
-                return False
-            # a has a right but b doesnt
-            if a.right and b.right == None:
-                return False
-            # a has a left but be doesnt
-            if a.left and b.left == None:
-                return False
-                   
+            # if either of them match then grab the children and append it so we can search
+            # and compare
+            if root_1.right and root_2.right:
+                list_1.append(root_1.right)
+                list_2.append(root_2.right)
+            if root_1.left and root_2.left:
+                list_1.append(root_1.left)
+                list_2.append(root_2.left)
+                
         return True
