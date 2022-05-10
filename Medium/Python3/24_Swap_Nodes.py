45727b0067622aa7678b4809e1364dada13d390a
unknown <cloudiebro@hotmail.com>
2022-05-09 22:47:41 -0700

    24 added method without using dummy node

--- Medium/Python3/24_Swap_Nodes.py
+++ Medium/Python3/24_Swap_Nodes.py
@@ -30,3 +30,64 @@ class Solution:
             second.next = first
             current = current.next.next
         return dummy.next
+
+# Note this is without dummy node
+class Solution1:
+    def swapPairs(self, head: ListNode) -> ListNode:
+        if not head or not head.next:
+            return head
+        
+        current = head
+        prev = head
+        
+        while current:
+            # example in the beginning we have 1 2 3 4 where head is 1 and current
+            if current == head:
+                # example 1 2 3 4
+                # head, current = 1
+                head = head.next # move head over one because it should be 2 after we swap
+                # head = 2
+                # current = 1
+                
+                current.next = head.next # current's next pointer now points to 3 since we will swap 1 and 2
+                # head = 2
+                # current = 1
+                # current.next = 3
+                # 2 3 4
+                #   |
+                #   1
+                
+                head.next = current # head which is 2 now, it's next pointer will now point to current which is 1
+                # head = 2
+                # current = 1
+                # current.next = 3
+                # 2 1 3 4
+                # head.next = 1
+                
+                prev = current # current is the previous node, which is 1
+            else:
+                # we are at the last node and it has no adjacent to swap, just return
+                if current.next == None:
+                    return head
+                
+                prev.next = current.next # 1 points to 4, 2 1 3 4, current is 3
+                # head = 2
+                # 2 1 4 -
+                #     |
+                #     3
+                #
+                
+                temp = current.next # hold the next pointer, in this case it's 4
+                current.next = current.next.next # disconnect from 3 and connect to none
+                # 2 1 4
+                # 3
+                #
+                #
+                
+                temp.next = current # 4 next pointer points to 3
+                # 2 1 4 3
+                prev = current # prev is 3 in this case 
+                
+            current = current.next # move to the next pair to swap
+                
+        return head
