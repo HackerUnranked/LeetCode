55f219fb84963faf9951ddd1b5c09cb2bd9f0104
unknown <cloudiebro@hotmail.com>
2022-07-18 17:52:36 -0700

    1309 initial commit

---
+++ Easy/Python3/1309_Decrypt_String_From_Alpha_Mapping.py
@@ -0,0 +1,46 @@
+# You are given a string s formed by digits and '#'. We want to map s to English lowercase characters as follows:
+
+#     Characters ('a' to 'i') are represented by ('1' to '9') respectively.
+#     Characters ('j' to 'z') are represented by ('10#' to '26#') respectively.
+
+# Return the string formed after mapping.
+
+# The test cases are generated so that a unique mapping will always exist.
+
+# Example 1:
+
+# Input: s = "10#11#12"
+# Output: "jkab"
+# Explanation: "j" -> "10#" , "k" -> "11#" , "a" -> "1" , "b" -> "2".
+
+# Example 2:
+
+# Input: s = "1326#"
+# Output: "acz"
+
+class Solution:
+    def freqAlphabets(self, s: str) -> str:
+        
+        # NOTE for dictionary in python 3.7 the insertion order is perserved!
+        # this is why if we have the string 110# we don't don't get the following:
+        #
+        # 1. a10#
+        # 2. aa0#
+        # 3. results = aa0# THIS IS WRONG
+        #
+        # because we have the "bigger values" inserted first in the dictionary then it becomes the following
+        #
+        # 1. 1j
+        # 2. aj
+        # 3. results = aj
+        
+        dic = { '10#':'j','11#':'k','12#':'l','13#':'m','14#':'n','15#':'o','16#':'p',
+               '17#':'q','18#':'r','19#':'s','20#':'t','21#':'u', '22#':'v','23#':'w',
+               '24#':'x','25#':'y','26#':'z', '1':'a','2':'b','3':'c','4':'d','5':'e',
+               '6':'f','7':'g','8':'h','9':'i' }
+        
+        # map each key to the val and replace
+        for key, val in dic.items():
+            s = s.replace(key, val)
+            
+        return s
