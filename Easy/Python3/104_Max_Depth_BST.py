3462d4014e709b5c36da5ae73dd650669de50b6b
cloud971 <samcha971@hotmail.com>
2024-06-29 18:35:55 -0700

    add queue method 104 max depth

--- Easy/Python3/104_Max_Depth_BST.py
+++ Easy/Python3/104_Max_Depth_BST.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 # Given a binary tree, find its maximum depth. The maximum depth is the number
 # of nodes along the longest path from the root node down to the farthest leaf node.
 
@@ -34,4 +36,42 @@ class Solution:
             return leftSub
         else:
             return rightSub
+    
+    # Shorter version of the above
+    def maxDepthEasy(self, root: TreeNode) -> int:
+        if root == None:
+            return 0
+        return max(self.maxDepth(root.right), self.maxDepth(root.left)) + 1
+
+    # Using a queue to find the max depth, this is BFS. BFS visits all the nodes
+    # at the current depth before moving on to the nodes at the next depth.
+    # Given binary tree [3,9,20,null,null,15,7],
+    #
+    #   3
+    #  / \
+    # 9  20
+    #    /  \
+    #   15   7
+    #
+    # 9 is visted first then we push its children to the queue,
+    # then 20 is visited and its children are pushed to the queue.
+    # The depth is incremented as we visit each node. Next we visit 15 and 7.
+    # and the process repeats until the queue is empty.  
+    #
+    def maxDepthQueue(self, root: TreeNode) -> int:    
+        if not root:
+            return 0   
+        queue = deque([(root, 1)])  # Queue of (node, depth)
+        max_depth = 0
         
+        while queue:
+            current_node, depth = queue.popleft()  # Get current node and its depth
+            max_depth = max(max_depth, depth)  # Update max depth
+            
+            # Add children to the queue with incremented depth
+            if current_node.left:
+                queue.append((current_node.left, depth + 1))
+            if current_node.right:
+                queue.append((current_node.right, depth + 1))
+        
+        return max_depth  
